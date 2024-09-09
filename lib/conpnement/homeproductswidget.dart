36ab768ab66019cce035781bedc70f19716d0fbd
unknown <youremail@gmail.com>
2024-09-09 18:56:31 +0300

    finalaly product and categories

--- lib/conpnement/homeproductswidget.dart
+++ lib/conpnement/homeproductswidget.dart
@@ -20,62 +20,72 @@ class Homeproductswidget extends ConsumerWidget {
                   itemBuilder: (context, index) {
                     return Padding(
                         padding: const EdgeInsets.all(2),
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          child: Container(
-                            width: 180,
-                            height: 180,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Column(
-                              children: [
-                                Image.network(
-                                  data[index].image,
-                                  fit: BoxFit.contain,
-                                  height: 150,
-                                ),
-                                const SizedBox(
-                                  height: 16,
-                                ),
-                                Center(
-                                  child: Text(
-                                    data[index].name,
-                                    style: const TextStyle(
-                                        color: Colors.grey,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        ProductDetailsPage(),
+                                        settings:RouteSettings(arguments: data[index]) ));
+                          },
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            child: Container(
+                              width: 180,
+                              height: 180,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Column(
+                                children: [
+                                  Image.network(
+                                    data[index].image,
+                                    fit: BoxFit.contain,
+                                    height: 150,
                                   ),
-                                ),
-                                const SizedBox(
-                                  height: 16,
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      "\$${data[index].price}",
+                                  const SizedBox(
+                                    height: 16,
+                                  ),
+                                  Center(
+                                    child: Text(
+                                      data[index].name,
                                       style: const TextStyle(
-                                          color: Colors.blue,
+                                          color: Colors.grey,
                                           fontWeight: FontWeight.bold,
                                           fontSize: 18),
                                     ),
-                                    Text(
-                                      "\$${data[index].oldPrice}",
-                                      style: const TextStyle(
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 18),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 16,
-                                ),
-                              ],
+                                  ),
+                                  const SizedBox(
+                                    height: 16,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        "\$${data[index].price}",
+                                        style: const TextStyle(
+                                            color: Colors.blue,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18),
+                                      ),
+                                      Text("\$${data[index].oldPrice}",
+                                          style: const TextStyle(
+                                              color: Colors.red,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18,
+                                              decoration:
+                                                  TextDecoration.lineThrough)),
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 16,
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ));
